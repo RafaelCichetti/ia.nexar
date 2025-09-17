3cc6d2b539d7edc4705afa7c291eae82c71466ae
RafaelCichetti <cichettirafael@gmail.com>
2025-09-17 14:20:08 -0300

    tentando fazer rodar no versel

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// Compat: tentar usar createRoot se disponível; caso contrário, usar ReactDOM.render (React 17)
+import * as ReactDOMCompat from 'react-dom/client';
+import * as ReactDOMLegacy from 'react-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+if (ReactDOMCompat && typeof ReactDOMCompat.createRoot === 'function') {
+  const root = ReactDOMCompat.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </React.StrictMode>
+  );
+} else if (ReactDOMLegacy && typeof ReactDOMLegacy.render === 'function') {
+  ReactDOMLegacy.render(
+    <React.StrictMode>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </React.StrictMode>,
+    container
+  );
+} else {
+  throw new Error('Nenhuma API de montagem ReactDOM disponível. Verifique versões de react e react-dom.');
+}
